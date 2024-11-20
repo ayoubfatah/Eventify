7c3ccbd5f9ed78538e03114a8d23eb423bd376bb
ayoubfatah <ayoubfatah222@gmail.com>
2024-11-20 13:21:09 +0100

    ,

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -2,18 +2,16 @@ import EventCard from "@/components/ui/EventCard";
 import { getEvent } from "@/lib/server-utils";
 import { notFound } from "next/navigation";
 
-type EventPageProps = {
-  params: {
-    slug: string;
-  };
+type EventParams = {
+  slug: string;
 };
 
-export async function generateMetadata({ params }: EventPageProps) {
+export async function generateMetadata({ params }: { params: EventParams }) {
   const { slug } = params;
   const event = await getEvent(slug);
   if (!event) return null;
 
-  return { title: ` ${event.name} event ` };
+  return { title: `${event.name} event` };
 }
 
 export async function generateStaticParams() {
@@ -25,14 +23,14 @@ export async function generateStaticParams() {
   ];
 }
 
-export default async function page({ params }: EventPageProps) {
-  const { slug } = await params;
+export default async function Page({ params }: { params: EventParams }) {
+  const { slug } = params;
   const event = await getEvent(slug);
 
   if (!event) return notFound();
 
   return (
-    <main className="h-[calc(100vh-8rem)]  flex justify-center items-center">
+    <main className="h-[calc(100vh-8rem)] flex justify-center items-center">
       <div className="w-[1100px]">
         <EventCard data={event} />
       </div>
